4a87a11b32a7c41e49ed2c46726698593bb7796e
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-13 10:41:29 +0100

    log out blog

--- views/main.js
+++ views/main.js
@@ -1,5 +1,6 @@
 var html = require('choo/html')
 var projects = require('../projects.json')
+var articles = require('../articles.json')
 
 module.exports = view
 
@@ -21,22 +22,41 @@ function view (state, emit) {
           <a class="fl mt2 white ml3" href="https://twitch.tv/yoshuawuyts">Twitch</a>
         </section>
         <section class="fl w-100 w-two-thirds-l pl6-ns">
-          <h2 class="mt5 mt0-ns">
-            Productions
-          </h2>
-          <section class="list pa0 cf">
-            ${projects.reverse().map(project => {
-              return html`
-                <div class="fl w-50-l mt2">
-                  <a href=${project.url} class="white">
-                    <strong>${project.name}</strong>
-                  </a>
-                  <span class="ml2 gray">${project.date}</span>
-                  <p class="mt1">${project.description}</p>
-                </div>
-              `
-            })}
-          </section>
+          <article>
+            <h2 class="mt5 mt0-ns">
+              Productions
+            </h2>
+            <section class="list pa0 cf">
+              ${projects.reverse().map(project => {
+                return html`
+                  <div class="fl w-50-l mt2">
+                    <a href=${project.url} class="white">
+                      <strong>${project.name}</strong>
+                    </a>
+                    <span class="ml2 gray">${project.date}</span>
+                    <p class="mt1">${project.description}</p>
+                  </div>
+                `
+              })}
+            </section>
+          </article>
+          <article class="w-100">
+            <h2 class="mt5">
+              Articles
+            </h2>
+            <section class="list pa0">
+              ${articles.reverse().map(article => {
+                return html`
+                  <div class="mt2">
+                    <a href=${article.url} class="white">
+                      <strong>${article.name}</strong>
+                    </a>
+                    <span class="ml2 gray">${article.date}</span>
+                  </div>
+                `
+              })}
+            </section>
+          </article>
         </section>
       </main>
     </body>
